448e8898b26d93f2b3634441279220a578177678
Brandon Green <brandon@brandonleeproductions.com>
2018-06-15 18:49:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,7 +12,7 @@ class Dog
     @name = name
   end
 
-  def self.all
+  def self.all  #Initially, I had an argument of (name), but that didn't work..
     @@all.each do |dog|
       puts dog.name
     end
