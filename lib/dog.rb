715892c55dbf94286e983792fe5c06bd045fa0e8
Brandon Green <brandon@brandonleeproductions.com>
2018-06-15 18:40:31 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  @@all = []
+
+  def initialize
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  
+
+end
